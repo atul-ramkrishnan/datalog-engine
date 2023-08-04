e895246c08fdcf4710bcbbb4b38105fef658ece8
atul-ramkrishnan <atul.ramkrishnan97@gmail.com>
2023-08-04 17:14:47 -0400

    Fix typo

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ from .engine.fixpoint import naive_evaluation, semi_naive_evaluation
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
-        raise TypeError(f"main takes excactly 1 argument ({len(sys.argv)} given)")
+        raise TypeError(f"main takes exactly 1 argument ({len(sys.argv)} given)")
     
     program = ""
     with open(sys.argv[1]) as f:
