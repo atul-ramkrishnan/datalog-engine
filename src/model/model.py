a28d3d1d2a563fcab77c578070e75face959c77b
atul-ramkrishnan <atul.ramkrishnan97@gmail.com>
2023-08-04 17:12:33 -0400

    Change folder structure

---
+++ src/model/model.py
@@ -0,0 +1,54 @@
+class Rule(object):
+    def __init__(self, head={}, body={}, type="rule"):
+        self.head = head 
+        self.body = body
+        self.type = type
+    def __repr__(self):
+        return "%r" % (self.__dict__)
+     
+
+class Fact(object):
+    def __init__(self, fact, type = "fact"):
+        self.fact = fact
+        self.fact.type = type
+        self.type = type
+        self.record = set()
+
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return self.__dict__ == other.__dict__
+        else:
+            return False
+
+    def __hash__(self):
+        return hash((self.fact.predicate, tuple(self.fact.terms), self.fact.isNegated, self.fact.type, self.type))
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __repr__(self):
+        return "%r" % (self.__dict__)
+
+
+class Predicate(object):
+    def __init__(self, name="", terms=[], isNegated=False, type = "predicate"):
+        self.predicate = name
+        self.terms = terms
+        self.isNegated = isNegated
+        self.type = type
+
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return self.__dict__ == other.__dict__
+        else:
+            return False
+    
+    def __hash__(self):
+        return hash((self.predicate, tuple(self.terms), self.isNegated, self.type))
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __repr__(self):
+        return "%r" % (self.__dict__)
+    
