d4b35351036c6e043a120ba1b99b91b63a7cd680
atul-ramkrishnan <atul.ramkrishnan97@gmail.com>
2023-08-04 17:14:29 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
 import sys
-from interpreter.tokenizer import Tokenizer
-from interpreter.parser import Parser
+from .interpreter.tokenizer import Tokenizer
+from .interpreter.parser import Parser
+import pprint
+from .engine.fixpoint import naive_evaluation, semi_naive_evaluation
+
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
@@ -17,5 +20,24 @@ if __name__ == '__main__':
 
     parser = Parser(tokenizer)
     parser.build()
-    parser.test(program)
+    # parser.test(program)
+
+    facts = []
+    rules = []
+    parsedProgram = parser.parser.parse(program)
+    # TODO: Handle syntax errors
+    if not parsedProgram:
+        print("ERROR")
+    for p in parsedProgram:
+        if p.type == 'fact':
+            facts.append(p)
+        elif p.type == 'rule':
+            rules.append(p)
+
+    pp = pprint.PrettyPrinter(depth=4)
+    # pp.pprint(facts)
+    # pp.pprint(rules)
+    # test(facts, rules)
+    database = semi_naive_evaluation(facts, rules)
+    print(database)
     
